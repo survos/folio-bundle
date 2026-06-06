d562ed2d906a65f573c6f440e3a7b3ea90348200
tacman <tacman@users.noreply.github.com>
2026-06-06 17:23:40 +0000

    allow value or raw for archives

--- src/Controller/FolioSearchController.php
+++ src/Controller/FolioSearchController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Survos\FolioBundle\Controller;
 
 use Survos\FolioBundle\Entity\Core;
+use Survos\FolioBundle\Entity\Folio;
 use Survos\FolioBundle\Service\FolioService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,6 +25,7 @@ final class FolioSearchController extends AbstractController
 
         return $this->render('@SurvosFolioBundle/folio/search.html.twig', [
             'ctx' => $ctx,
+            'folio' => $ctx->em->find(Folio::class, $ctx->folioCode),
             'cores' => $ctx->em->getRepository(Core::class)->findBy([], ['code' => 'ASC']),
         ]);
     }
